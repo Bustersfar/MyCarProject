257a82332da628ba65835490ea0f139b0065b1cf
Henrik Carlsen <henrikca@gmail.com>
2026-02-12 13:23:34 +0100

    Added code to calculate different stuff about the car

--- CarApp/CarApp/Program.cs
+++ CarApp/CarApp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Indtast bilmærke: ");
+            /* Console.Write("Indtast bilmærke: ");
             string brand = Console.ReadLine();
             Console.Write("Indtast bilmodel: ");
             string model = Console.ReadLine();
@@ -12,12 +12,35 @@
             int year = Convert.ToInt32(Console.ReadLine());
             Console.Write("Indtast geartype (A for automatisk, M for manuel: ");
             char gearType = Console.ReadLine()[0];
+
             Console.WriteLine("Bilmærke: " + brand);
             Console.WriteLine("Bilmodel: " + model);
             Console.WriteLine("Årgang: " + year);
             Console.WriteLine("Gear: " + gearType);
-            Console.WriteLine($"\n{brand} {model} fra {year} har gear {gearType}.");
-            Console.WriteLine(brand + " " + model + " fra " + year + " har gear: " + gearType);
+            Console.WriteLine($"\n{brand} {model} fra {year} har gear {gearType}."); */
+            
+            Console.Write("Indtast brændstoftype (D for diesel, B for benzin: ");
+            char fuelType = Console.ReadLine()[0];
+            Console.Write("Indtast Km/l: ");
+            int kml = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kilometerstand: ");
+            int mileage = Convert.ToInt32(Console.ReadLine());
+            double gasPrice = 13.49; // Eksempel på benzinpris pr. liter
+            double dieselPrice = 12.29; // Eksempel på dieselpris pr. liter
+            Console.Write("Indtast distance: ");
+            double distance = double.Parse(Console.ReadLine());
+            double fuelNeeded = distance / kml; // Beregn brændstofbehovet
+            double totalCost = 0;
+            if (fuelType == 'B' || fuelType == 'b')
+            {
+                totalCost = fuelNeeded * gasPrice;
+            }
+            else
+            {
+                totalCost = fuelNeeded * dieselPrice;
+            }
+            Console.WriteLine($"Brændstoforbrug for {distance} km: {fuelNeeded} liter");
+            Console.WriteLine($"Brændstofomkostninger for {distance} km: {totalCost:C}");
         }
     }
 }
--- CarApp/CarApp/obj/Debug/net10.0/CarApp.AssemblyInfo.cs
+++ CarApp/CarApp/obj/Debug/net10.0/CarApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858d73d0179cab735b5806b41171f5c8428f13d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d66a73db933fed4adebdad62f5ae0aad9e5bf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
