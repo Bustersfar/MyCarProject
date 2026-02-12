5e37bf5a5f32086c1f149afa4e9bf652b124624e
Henrik Carlsen <henrikca@gmail.com>
2026-02-12 11:24:47 +0100

    Added .gitignore

--- CarApp/CarApp/Program.cs
+++ CarApp/CarApp/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Indtast bilmærke: ");
+            string brand = Console.ReadLine();
+            Console.Write("Indtast bilmodel: ");
+            string model = Console.ReadLine();
+            Console.Write("Indtast årgang: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Indtast geartype (A for automatisk, M for manuel: ");
+            char gearType = Console.ReadLine()[0];
+            Console.WriteLine("Bilmærke: " + brand);
+            Console.WriteLine("Bilmodel: " + model);
+            Console.WriteLine("Årgang: " + year);
+            Console.WriteLine("Gear: " + gearType);
+            Console.WriteLine($"\n{brand} {model} fra {year} har gear {gearType}.");
+            Console.WriteLine(brand + " " + model + " fra " + year + " har gear: " + gearType);
         }
     }
 }
